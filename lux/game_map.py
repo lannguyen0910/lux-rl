cbf7b42f646f60d5a716c5b9bef065736e535a98
lannguyen <18120051@student.hcmus.edu.vn>
2021-09-04 07:50:18 +0700

    update game map

--- lux/game_map.py
+++ lux/game_map.py
@@ -1,9 +1,9 @@
-import math
 from typing import List
-
+from .constants import Constants
+from .game_objects import CityTile, Unit
+from .game_position import Position
 from .constants import Constants
 
-DIRECTIONS = Constants.DIRECTIONS
 RESOURCE_TYPES = Constants.RESOURCE_TYPES
 
 
@@ -17,8 +17,10 @@ class Cell:
     def __init__(self, x, y):
         self.pos = Position(x, y)
         self.resource: Resource = None
-        self.citytile = None
+        self.citytile: CityTile = None
+        self.unit: Unit = None
         self.road = 0
+
     def has_resource(self):
         return self.resource is not None and self.resource.amount > 0
 
@@ -45,62 +47,3 @@ class GameMap:
         """
         cell = self.get_cell(x, y)
         cell.resource = Resource(r_type, amount)
-
-
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __sub__(self, pos) -> int:
-        return abs(pos.x - self.x) + abs(pos.y - self.y)
-
-    def distance_to(self, pos):
-        """
-        Returns Manhattan (L1/grid) distance to pos
-        """
-        return self - pos
-
-    def is_adjacent(self, pos):
-        return (self - pos) <= 1
-
-    def __eq__(self, pos) -> bool:
-        return self.x == pos.x and self.y == pos.y
-
-    def equals(self, pos):
-        return self == pos
-
-    def translate(self, direction, units) -> 'Position':
-        if direction == DIRECTIONS.NORTH:
-            return Position(self.x, self.y - units)
-        elif direction == DIRECTIONS.EAST:
-            return Position(self.x + units, self.y)
-        elif direction == DIRECTIONS.SOUTH:
-            return Position(self.x, self.y + units)
-        elif direction == DIRECTIONS.WEST:
-            return Position(self.x - units, self.y)
-        elif direction == DIRECTIONS.CENTER:
-            return Position(self.x, self.y)
-
-    def direction_to(self, target_pos: 'Position') -> DIRECTIONS:
-        """
-        Return closest position to target_pos from this position
-        """
-        check_dirs = [
-            DIRECTIONS.NORTH,
-            DIRECTIONS.EAST,
-            DIRECTIONS.SOUTH,
-            DIRECTIONS.WEST,
-        ]
-        closest_dist = self.distance_to(target_pos)
-        closest_dir = DIRECTIONS.CENTER
-        for direction in check_dirs:
-            newpos = self.translate(direction, 1)
-            dist = target_pos.distance_to(newpos)
-            if dist < closest_dist:
-                closest_dir = direction
-                closest_dist = dist
-        return closest_dir
-
-    def __str__(self) -> str:
-        return f"({self.x}, {self.y})"
