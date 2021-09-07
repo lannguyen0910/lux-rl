cb5deadb7db6685b66ed75083c36cb826a7952b3
lannguyen <18120051@student.hcmus.edu.vn>
2021-09-07 12:37:50 +0700

    smarter unit

--- lux/find_cluster.py
+++ lux/find_cluster.py
@@ -17,26 +17,34 @@ def find_best_cluster(game_state: Game, unit: Unit, distance_multiplier=-0.1, DE
     else:
         print = lambda *args: None
 
+    # [TODO] put logic in units
     cooldown = GAME_CONSTANTS["PARAMETERS"]["UNIT_ACTION_COOLDOWN"]["WORKER"]
     travel_range = max(1, game_state.turns_to_night // cooldown - 2)
 
     if unit.night_turn_survivable > game_state.turns_to_dawn and not game_state.is_day_time:
-        travel_range = game_state.map_width + game_state.map_height
+        travel_range = GAME_CONSTANTS["PARAMETERS"]["DAY_LENGTH"] // cooldown + \
+            unit.night_travel_range
 
     if unit.night_turn_survivable > GAME_CONSTANTS["PARAMETERS"]["NIGHT_LENGTH"]:
-        travel_range = game_state.map_width + game_state.map_height
-
-    # [TODO] consider the resources carried as well
-    # [TODO] fix bug regarding nighttime travel, but just let them die perhaps
+        travel_range = GAME_CONSTANTS["PARAMETERS"]["DAY_LENGTH"] // cooldown + \
+            unit.night_travel_range
 
     score_matrix_wrt_pos = game_state.init_zero_matrix()
 
     best_position = unit.pos
     best_cell_value = -1
 
-    # design a good game's matrix
-    matrix = game_state.calculate_dominance_matrix(
-        game_state.convolved_rate_matrix)
+    # if current cluster size has more than one agent mining
+    consider_different_cluster = False
+    current_leader = game_state.xy_to_resource_group_id.find(tuple(unit.pos))
+    if current_leader:
+        units_mining_on_current_cluster = game_state.resource_leader_to_locating_units[
+            current_leader]
+        if len(units_mining_on_current_cluster) > 1:
+            consider_different_cluster = True
+
+    # give very slight preference to richer matrices
+    matrix = game_state.convolved_rate_matrix**0.01
 
     for y in range(game_state.map_height):
         for x in range(game_state.map_width):
@@ -50,10 +58,19 @@ def find_best_cluster(game_state: Game, unit: Unit, distance_multiplier=-0.1, DE
                 continue
 
             # [TODO] make it smarter than random
-            target_bonus = 1.5
-            if distance_multiplier > 0:
-                if game_state.xy_to_resource_group_id.find((x, y),) in game_state.targeted_leaders:
-                    target_bonus = 1
+
+            # if the targeted cluster is not targeted and mined
+            # prefer to target the other cluster
+            target_bonus = 1
+            if consider_different_cluster:
+                target_leader = game_state.xy_to_resource_group_id.find((x, y))
+                if target_leader:
+                    units_targeting_or_mining_on_target_cluster = \
+                        game_state.resource_leader_to_locating_units[target_leader] | \
+                        game_state.resource_leader_to_targeting_units[target_leader]
+
+                    if len(units_targeting_or_mining_on_target_cluster) == 0:
+                        target_bonus = 10
 
             empty_tile_bonus = 1
             if game_state.distance_from_resource[y, x] == 1:
@@ -66,8 +83,8 @@ def find_best_cluster(game_state: Game, unit: Unit, distance_multiplier=-0.1, DE
                 distance = max(1, dx + dy)
                 if distance <= travel_range:
                     # encourage going far away
-                    # [TODO] discourage returning to explored territory
-                    # [TODO] discourage going to planned locations
+                    # discourage returning to explored territory
+                    # discourage going to planned locations
                     cell_value = target_bonus * empty_tile_bonus * \
                         matrix[y, x] * distance ** distance_multiplier
                     score_matrix_wrt_pos[y, x] = int(cell_value)
@@ -76,7 +93,7 @@ def find_best_cluster(game_state: Game, unit: Unit, distance_multiplier=-0.1, DE
                         best_cell_value = cell_value
                         best_position = Position(x, y)
 
-    print(travel_range)
-    print(score_matrix_wrt_pos)
+    # print(travel_range)
+    # print(score_matrix_wrt_pos)
 
     return best_position, best_cell_value
