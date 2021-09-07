e6383d4ae2e6df46e37ca4a6f6826d864d7961b1
lannguyen <18120051@student.hcmus.edu.vn>
2021-09-07 07:36:38 +0700

    design good matrix

--- lux/find_cluster.py
+++ lux/find_cluster.py
@@ -1,8 +1,9 @@
 import builtins as __builtin__
 import numpy as np
+import math
+import random
 
 from typing import List
-
 from .game import Game, Player
 from .game_map import Cell, RESOURCE_TYPES, Position
 from .constants import Constants
@@ -19,22 +20,37 @@ def find_best_cluster(game_state: Game, position: Position, distance_multiplier=
     width, height = game_state.map_width, game_state.map_height
 
     cooldown = GAME_CONSTANTS["PARAMETERS"]["UNIT_ACTION_COOLDOWN"]["WORKER"]
-    travel_range = max(0, game_state.turns_to_night // cooldown - 2)
+    travel_range = max(1, game_state.turns_to_night // cooldown - 2)
+    # [TODO] consider the resources carried as well
+    # [TODO] fix bug regarding nighttime travel, but just let them die perhaps
 
     score_matrix_wrt_pos = [[0 for _ in range(width)] for _ in range(height)]
 
     best_position = position
     best_cell_value = -1
+    polar_offset = random.uniform(0, math.pi)
 
-    for y, row in enumerate(game_state.resource_maxpool_matrix):
-        for x, maxpool_scores in enumerate(row):
-            if maxpool_scores > 0:
-                distance = max(1, abs(position.x - x) + abs(position.y - y))
+    # design a good game's matrix
+    matrix = game_state.calculate_dominance_matrix(
+        game_state.convolved_rate_matrix)
+
+    for y, row in enumerate(matrix):
+        for x, scores in enumerate(row):
+            # [TODO] make it smarter than random
+
+            # add random preferences in the directions
+            dx, dy = abs(position.x - x), abs(position.y - y)
+            polar_factor = math.sin(math.atan2(dx, dy) + polar_offset)**2
+            if game_state.turn < 30:  # not so much
+                polar_factor = math.sqrt(polar_factor)
+
+            if scores > 0:
+                distance = max(1, dx + dy)
                 if distance <= travel_range:
                     # encourage going far away
                     # [TODO] discourage returning to explored territory
                     # [TODO] discourage going to planned locations
-                    cell_value = maxpool_scores * distance ** distance_multiplier
+                    cell_value = polar_factor * scores * distance ** distance_multiplier
                     score_matrix_wrt_pos[y][x] = int(cell_value)
 
                     if cell_value > best_cell_value:
