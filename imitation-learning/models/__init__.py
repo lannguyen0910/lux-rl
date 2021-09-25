a3fa24ec6a1e2ff08484beade67dce2dd6944e2e
lannguyen <18120051@student.hcmus.edu.vn>
2021-09-25 11:57:22 +0700

    lux model

---
+++ imitation-learning/models/__init__.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicConv2d(nn.Module):
+    def __init__(self, input_dim, output_dim, kernel_size, bn):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            input_dim, output_dim, 
+            kernel_size=kernel_size, 
+            padding=(kernel_size[0] // 2, kernel_size[1] // 2)
+        )
+        self.bn = nn.BatchNorm2d(output_dim) if bn else None
+
+    def forward(self, x):
+        h = self.conv(x)
+        h = self.bn(h) if self.bn is not None else h
+        return h
+
+
+class LuxNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        layers, filters = 12, 32
+        self.conv0 = BasicConv2d(20, filters, (3, 3), True)
+        self.blocks = nn.ModuleList([BasicConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
+        self.head_p = nn.Linear(filters, 5, bias=False)
+
+    def forward(self, x):
+        h = F.relu_(self.conv0(x))
+        for block in self.blocks:
+            h = F.relu_(h + block(h))
+        h_head = (h * x[:,:1]).view(h.size(0), h.size(1), -1).sum(-1)
+        p = self.head_p(h_head)
+        return p
